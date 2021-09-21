b044af555c2956eda163b20a5fdbf0fa3aa7e794
Alireza Razinejad <a.razinejad@gmail.com>
2021-09-21 20:32:09 +0430

    feat: Send props to Header

--- src/App.js
+++ src/App.js
@@ -5,10 +5,15 @@ import Content from './components/Content';
 import Footer from './components/Footer';
 
 export default class App extends Component {
+    headerProps = {
+        title: 'Cool APP',
+        favorite_color: 'blue'
+    }
+
   render() {
       return (
           <Fragment>
-              <Header />
+              <Header {...this.headerProps} />
               <Content />
               <Footer />
           </Fragment>
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ export default class Header extends Component {
     render() {
         return (
             <nav className={'flex justify-evenly bg-gray-800 bg-gradient-to-l p-2 shadow-md fixed top-0 left-0 right-0'}>
+                <h2 className={'text-blue-50 hover:text-blue-400'}>{this.props.title}</h2>
                 <a className={'text-blue-50 hover:text-blue-400'} href="#">Home</a>
                 <a className={'text-blue-50 hover:text-blue-400'} href="#">About</a>
                 <a className={'text-blue-50 hover:text-blue-400'} href="#">Contact</a>
